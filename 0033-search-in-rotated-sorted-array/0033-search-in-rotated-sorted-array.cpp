80663023a02d63e433624a199acdf8843a6d066b
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-25 07:59:16 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (37.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        //Approach 2 Binary Search
+        int low=0, high=nums.size()-1;
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                return i;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            
+            if(nums[mid]==target) return mid;
+            
+            if(nums[low]<=nums[mid]){
+                if(target<=nums[mid] && nums[low]<=target){
+                    high=mid-1;
+                }else {
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }
         }
         return -1;
+        
+        
+        
+        
+        
+        
+        // Approach 1 Linear Search O(n)
+        
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
 };
