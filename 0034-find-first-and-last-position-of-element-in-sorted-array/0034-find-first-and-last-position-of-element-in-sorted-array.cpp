d71394da544eec6171a8e3cd0c384a558ff8180f
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-25 06:59:59 +0530

    Time: 0 ms (100.00%), Space: 16.1 MB (13.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,52 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        //Approach 2
-        int left=-1, right=-1;
-        
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                if(left==-1) left=i;
-                
-                right=i;
+    int Righti(vector<int>& nums, int target){
+        int low=0, high=nums.size()-1;
+        int last=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target){
+                last=mid, low=mid+1;
+            }else if(nums[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
-        return {left,right};
+        return last;
+    }
+    int Lefti(vector<int>& nums, int target){
+        int low=0, high=nums.size()-1;
+        int first=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]==target){
+                first=mid, high=mid-1;
+            }else if(nums[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return first;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+      return {Lefti(nums,target), Righti(nums,target)};
+        
+        
+        //Approach 2
+//         int left=-1, right=-1;
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==target){
+//                 if(left==-1) left=i;
+                
+//                 right=i;
+//             }
+//         }
+//         return {left,right};
         
         //Approach 1
         
