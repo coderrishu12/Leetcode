e89080a1b00e8eeaa165e4a41586526dcee68be2
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-26 08:48:05 +0530

    Time: 3 ms (79.85%), Space: 16.5 MB (73.58%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,14 +1,44 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+//         Approach 2 Binary Search
+        int low=0, high=nums.size()-1;
         
-        for(auto &ele:nums){
-            mp[ele]++;
-            if(mp[target ]>=1 ){
-                return true;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return true;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++, high--;
+                continue;
+            }
+            
+            if(nums[low]<=nums[mid]){
+                if(target<=nums[mid] && nums[low]<=target){
+                    high=mid-1;
+                }else {
+                    low=mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }
         }
         return false;
+        
+        
+        
+//          Approach 1 (Brute Force)
+//         unordered_map<int, int> mp;
+        
+//         for(auto &ele:nums){
+//             mp[ele]++;
+//             if(mp[target ]>=1 ){
+//                 return true;
+//             }
+//         }
+//         return false;
     }
 };
