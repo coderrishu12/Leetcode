1942c34ce00880ddd91e1c98b40c975e4fee1e87
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-27 11:06:14 +0530

    Time: 6 ms (31.04%), Space: 11.2 MB (50.94%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -13,8 +13,11 @@ public:
             
             if(nums[mid]>nums[mid-1]){
                 low=mid+1;
-            }else{
+            }else if(nums[mid]>nums[mid+1]){
                 high=mid-1;
+            }else{
+                low=mid+1;
+                
             }
         }
         return 0;
