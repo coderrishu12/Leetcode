aa1850a54d6fd2c8bf7332bb2fea77a5e40fe678
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-27 10:28:37 +0530

    Time: 15 ms (84.90%), Space: 24.7 MB (51.61%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -10,20 +10,13 @@ public:
             int mid=low+(high-low)/2;
             cout<<mid<<" ";
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            if(mid%2!=0 ){
-                if(nums[mid]==nums[mid-1]){    
+            if((mid%2!=0 && nums[mid]==nums[mid-1])||(mid%2==0 && nums[mid]==nums[mid+1])){
                     low=mid+1;    
-                    
-                }else{
-                    high=mid-1;
-                }
+           
             }else{
-                if(nums[mid]==nums[mid+1]){    
-                         low=mid+1;  
-                }else{
                    high=mid-1;
-                }
             }
+            
           
             
         }
