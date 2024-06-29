24ce2ebc230585747a90c7f275244d4120477079
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-29 07:51:35 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (71.87%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int canAccumulate(vector<int>& nums, int mid){
+        int cnt=1, splitsSum=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]+splitsSum<=mid){
+                splitsSum+=nums[i];
+            }else{
+                cnt++;
+                splitsSum=nums[i];
+            }
+        }
+        return cnt;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end()), high=accumulate(nums.begin(), nums.end(), 0);
+        int ans=-1;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if(canAccumulate(nums, mid)<=k){
+                ans= mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
