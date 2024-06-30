109178456ccd465c01d2907413203d537756090b
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-06-30 16:10:05 +0530

    Time: 8 ms (21.89%), Space: 11.9 MB (49.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    bool lowerBound(vector<int>& arr, int n, int x){
-        int low=0, high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+//     bool lowerBound(vector<int>& arr, int n, int x){
+//         int low=0, high=n-1;
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
             
-            if(arr[mid]==x) return true;
-            if(arr[mid]<x) low=mid+1;
-            else high=mid-1;
-        }
-        return false;
-    }
+//             if(arr[mid]==x) return true;
+//             if(arr[mid]<x) low=mid+1;
+//             else high=mid-1;
+//         }
+//         return false;
+//     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix[0].size();
         int n=matrix.size();
-        for(int i=0;i<n;i++){
-            if(lowerBound(matrix[i],m, target)) return true;;
+//         Approach 2(Binary Search)
+        
+        int low=0, high=(n*m-1);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m, col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
         }
         return false;
+        
+        
+        
+        //  Approach 1
+        // for(int i=0;i<n;i++){
+        //     if(lowerBound(matrix[i],m, target)) return true;;
+        // }
+        // return false;
     }
 };
