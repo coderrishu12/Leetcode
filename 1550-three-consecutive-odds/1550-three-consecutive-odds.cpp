3dfe3c895798c1836aca8d71e920273e572e5ce3
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-01 12:41:34 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (45.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        
-       
-        int cnt=0;
-        for(int i=0;i<arr.size();i++){
+        if(arr.size()<3) return false;
+        for(int i=0;i<arr.size()-2;i++){
             
-                if(arr[i]%2==1) cnt++;
-                else cnt=0;
-            if(cnt==3) return true;
+                if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1) return true;
+                
+            
+           
         }
         return false;
         
