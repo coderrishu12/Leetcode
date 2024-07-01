c1d61a49cbd0b74aa75208cf28c18cb282e687ca
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-01 09:06:12 +0530

    Time: 14 ms (56.31%), Space: 23.2 MB (51.10%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -11,22 +11,42 @@
  */
 class Solution {
 public:int maxDepth;
-    int bottomLeft;
-    void DFS(TreeNode* root, int currDepth){
-        if(!root) return ;
+//     int bottomLeft;
+//     void DFS(TreeNode* root, int currDepth){
+//         if(!root) return ;
         
-        if(currDepth>maxDepth){
-            maxDepth=currDepth;
-            bottomLeft=root->val;
-        }
-        DFS(root->left, currDepth+1);
-        DFS(root->right, currDepth+1);
+//         if(currDepth>maxDepth){
+//             maxDepth=currDepth;
+//             bottomLeft=root->val;
+//         }
+//         DFS(root->left, currDepth+1);
+//         DFS(root->right, currDepth+1);
 
-    }
+//     }
     int findBottomLeftValue(TreeNode* root) {
+//         Approach 2 BFS
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxi=-1;
+        while(!q.empty()){
+            TreeNode* root = q.front();
+            q.pop();
+            maxi=root->val;
+            
+            if(root->right){
+                q.push(root->right);
+            }
+            if(root->left){
+                q.push(root->left);
+            }
+        }
+        return maxi;
+        
+        
 //         Approach 1 DFS
-        maxDepth=-1;
-        DFS(root, 0);
-        return bottomLeft;
+        
+        // maxDepth=-1;
+        // DFS(root, 0);
+        // return bottomLeft;
     }
 };
