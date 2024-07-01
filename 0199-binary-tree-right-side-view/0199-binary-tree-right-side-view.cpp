a0e44c6092f788af9c1b01d759504bd4b4b88103
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-01 07:41:36 +0530

    Time: 3 ms (57.11%), Space: 13.9 MB (73.60%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,33 +11,49 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
-        if(!root) return res;
-        map<int, int> mp;
-        queue<pair<TreeNode* , int>> q;
-        q.push({root, 0});
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            
-            TreeNode* node = it.first;
-            int x=it.second;
-             if (mp.find(x) == mp.end()) {
-                mp[x] = node->val;
-             }
-            
-            if(node->right){
-                q.push({node->right, x+1});
-            }
-            if(node->left){
-                q.push({node->left, x + 1});
-            }
-        }        
-        
-        for(auto it: mp){
-            res.push_back(it.second);
+    void findRight(TreeNode* root,int level, vector<int>& res){
+        if(!root) return;
+        if(res.size()==level){
+            res.push_back(root->val);
         }
+        findRight(root->right,level+1, res);
+        findRight(root->left,level+1, res);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+//         Approach 2 Recursion
+        vector<int> res;
+        findRight(root,0, res);
         return res;
+        
+        
+//          Approach 1    
+        
+//         vector<int> res;
+//         if(!root) return res;
+//         map<int, int> mp;
+//         queue<pair<TreeNode* , int>> q;
+//         q.push({root, 0});
+//         while(!q.empty()){
+//             auto it=q.front();
+//             q.pop();
+            
+//             TreeNode* node = it.first;
+//             int x=it.second;
+//              if (mp.find(x) == mp.end()) {
+//                 mp[x] = node->val;
+//              }
+            
+//             if(node->right){
+//                 q.push({node->right, x+1});
+//             }
+//             if(node->left){
+//                 q.push({node->left, x + 1});
+//             }
+//         }        
+        
+//         for(auto it: mp){
+//             res.push_back(it.second);
+//         }
+//         return res;
     }
 };
