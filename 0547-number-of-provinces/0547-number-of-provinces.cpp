4feee71187eef391a523d9ab5b09103e0b538ae2
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-01 16:19:26 +0530

    Time: 21 ms (60.52%), Space: 21.3 MB (8.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:int n;
+    void BFS(vector<vector<int>>& isConnected, int node, vector<bool> &vis){
+        queue<int> q;
+        q.push(node);
+        vis[node]=true;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            
+            for(int i=0;i<n;i++){
+                if(isConnected[it][i]==1 && !vis[i] ){
+                    BFS(isConnected, i, vis);
+                }
+            }
+            
+        }
+    }
     void DFS(vector<vector<int>>& isConnected, int node, vector<bool> &vis){
         vis[node]=1;
 
@@ -16,7 +32,7 @@ public:int n;
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 cnt++;
-                DFS(isConnected, i, vis);
+                BFS(isConnected, i, vis);
             }
         }
         return cnt;
