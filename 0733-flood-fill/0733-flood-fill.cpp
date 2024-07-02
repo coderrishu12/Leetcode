b9734a7fcd3a71b90ce8a73c7d31824af7257809
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-02 09:45:08 +0530

    Time: 12 ms (12.72%), Space: 16.6 MB (84.29%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void DFS(vector<vector<int>>& grid,vector<vector<int>>& ans, int sr, int sc, int &color, int &temp, int drow[], int dcol[]){
+        ans[sr][sc]=color;
+        int n = grid.size();
+        int m = grid[0].size();
+       
+       
+            for(int i=0;i<4;i++){
+                int nrow = sr+drow[i];
+                int ncol = sc+dcol[i];
+                if(nrow>=0 && nrow<n && ncol >=0 && ncol<m && ans[nrow][ncol]!=color && grid[nrow][ncol]==temp){
+                   
+                    DFS(grid, ans, nrow,ncol, color, temp , drow, dcol);
+                }
+            }
+        }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        int temp=image[sr][sc];
+        vector<vector<int>> ans=image;
+         int drow[] = {-1,0,+1,0};
+         int dcol[] = {0,+1,0,-1};
+            DFS(image, ans, sr,sc, color, temp , drow, dcol);
+        
+        return ans;
+    }
+};
