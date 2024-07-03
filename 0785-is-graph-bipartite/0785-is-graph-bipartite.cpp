27e943a332bac4e48eae68e9d595b17f3d871194
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-03 09:59:07 +0530

    Time: 14 ms (86.06%), Space: 16 MB (94.49%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -20,12 +20,29 @@ public:
         }
         return true;
     }
+    
+    bool checkBipartiteDFS(vector<vector<int>>& graph, int curr, vector<int>& color, int currColor){
+        color[curr]=currColor;
+            
+            for(auto v: graph[curr]){
+                if(color[v]==-1){
+                    color[v]=1-color[curr];
+                    if(checkBipartiteDFS(graph ,v,color, color[v])==false){
+                        return false;
+                    }
+                }else if(color[v]==color[curr]){
+                    return false;
+                }
+            }
+        
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int> color(n,-1);
             for(int i=0;i<n;i++){
                 if(color[i]==-1){
-                 if(checkBipartiteBFS(graph, i, color, 1)==false){
+                 if(checkBipartiteDFS(graph, i, color, 1)==false){
                      return false;
                  }   
                 }
