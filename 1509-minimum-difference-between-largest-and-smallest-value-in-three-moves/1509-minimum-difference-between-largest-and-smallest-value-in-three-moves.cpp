5b06ac59f5a0b4c5d94ec6d0ddfec82adf0f6fa7
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-03 18:02:00 +0530

    Time: 72 ms (39.46%), Space: 38.8 MB (50.84%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -5,13 +5,8 @@ public:
         if(nums.size()<=4) return 0;
         
         sort(nums.begin(), nums.end());
-        int mini1= nums[n-4]-nums[0];
-        int mini2= nums[n-3]-nums[1];       
-        int mini3= nums[n-2]-nums[2];
-        int mini4= nums[n-1]-nums[3];
         
-        int mini5 = min(min(mini1,mini2), min(mini3,mini4));
-        return mini5;
+        return min(min(nums[n-4]-nums[0], nums[n-3]-nums[1]), min(nums[n-2]-nums[2],nums[n-1]-nums[3]));
         
     }
 };
