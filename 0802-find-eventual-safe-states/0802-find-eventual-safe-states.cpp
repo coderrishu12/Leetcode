d4bfb24d685a862fdb2fe035bf52d3bfa1a9f2fa
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-03 12:47:12 +0530

    Time: 102 ms (90.79%), Space: 50.6 MB (97.94%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool isCyclicDFS(vector<vector<int>>& graph,  vector<int>& vis, vector<int>& dfsVis, int u){
-        vis[u]=1;
-        dfsVis[u]=1;
+    bool isCyclicDFS(vector<vector<int>>& graph,  vector<int>& vis, int u){
+        vis[u]=2;
+        
         
         for(auto &v: graph[u]){
-            if(vis[v]==0 && isCyclicDFS(graph, vis, dfsVis, v)==true){
+            if(vis[v]==0 && isCyclicDFS(graph, vis, v)==true){
                 return true;
-            }else if(dfsVis[v]==1){
+            }else if(vis[v]==2){
                 return true;
             }
         }
-        dfsVis[u]=0;
+        vis[u]=1;
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
         
         vector<int> vis(n,0);
-        vector<int> dfsVis(n,0);
+        
         
         for(int i=0;i<n;i++){
-            if(vis[i]==0 && isCyclicDFS(graph, vis, dfsVis, i)==0){
+            if(vis[i]==0 && isCyclicDFS(graph, vis, i)==0){
                 continue;
             }
         }
         vector<int> ans;
         for(int i=0;i<n;i++){
-            if(dfsVis[i]==0){
+            if(vis[i]==1){
                 ans.push_back(i);
             }
         }
