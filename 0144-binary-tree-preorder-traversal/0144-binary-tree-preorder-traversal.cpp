a56175374e28bc6961874d9e86973689b11dd4e0
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-05 12:05:51 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (96.70%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -21,20 +21,50 @@ public:
     //     preOrderTraversal(root->right, ans);
     // }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> preorder;
-        if(root==NULL) return preorder;
-        
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            root=st.top();
-            st.pop();
-            
-            preorder.push_back(root->val);
-            if(root->right) st.push(root->right);
-            if(root->left) st.push(root->left);
+        //    Approach 3 without recursion
+         vector<int> preorder;
+        TreeNode* curr= root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                preorder.push_back(curr->val);
+                curr=curr->right;
+            }else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr=curr->left;
+                }else{
+                    prev->right = NULL;
+                    curr=curr->right;
+                }
+            }
         }
         return preorder;
+                
+         
+        
+//         Approach 2
+//         vector<int> preorder;
+//         if(root==NULL) return preorder;
+        
+//         stack<TreeNode*> st;
+//         st.push(root);
+//         while(!st.empty()){
+//             root=st.top();
+//             st.pop();
+            
+//             preorder.push_back(root->val);
+//             if(root->right) st.push(root->right);
+//             if(root->left) st.push(root->left);
+//         }
+//         return preorder;
+        
+        
         // vector<int> ans;
         // preOrderTraversal(root, ans);
         // return ans;
