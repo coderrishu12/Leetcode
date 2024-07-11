d4fab512ea16102287a306f300204025845f8f11
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-11 08:53:05 +0530

    Time: 298 ms (5.02%), Space: 82.6 MB (5.04%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,46 +1,89 @@
 class Solution {
 public:
     vector<vector<int>> directions{{-1,-1},{-1,0}, {-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+    typedef pair<int, pair<int, int>> P;
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+//         Approach 2 by Dijkstra
+            int m=grid.size();
+            int n=grid[0].size();
+
+            if(m==0 || n==0 || grid[0][0]!=0){
+                return -1;
+            }
+
+            auto isSafe=[&](int x, int y){
+                return x>=0 && x<m && y>=0 && y<n;
+
+            };
+            vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+            priority_queue<P, vector<P>, greater<P>> pq;
+            pq.push({0, {0,0}});
+            result[0][0]=0;
         
-        if(m==0 || n==0 || grid[0][0]!=0){
-            return -1;
-        }
-        
-        auto isSafe=[&](int x, int y){
-            return x>=0 && x<m && y>=0 && y<n;
-               
-        };
-        queue<pair<int, int>> q;
-        q.push({0,0});
-        grid[0][0]=1;
-        
-        int level=0;
-        while(!q.empty()){
-            int N=q.size();
-            while(N--){
-                auto curr=q.front();
-                q.pop();
-                int x=curr.first;
-                int y=curr.second;
+        while(!pq.empty()){
+            int d = pq.top().first;
+            pair<int, int> node = pq.top().second;
+            int x= node.first;
+            int y= node.second;
+            pq.pop();
+            
+            for(auto dir: directions){
+                int x_ = x+ dir[0];
+                int y_ = y+ dir[1];
                 
-                if(x==m-1 && y==n-1){
-                    return level+1;
-                }
-                for(auto dir: directions){
-                    int _x = x+dir[0];
-                    int _y = y+dir[1];
-                    
-                    if(isSafe(_x,_y) && grid[_x][_y]==0){
-                        q.push({_x,_y});
-                        grid[_x][_y]=1;
-                    }
+                int dist=1;
+                
+                if(isSafe(x_, y_) && grid[x_][y_]==0 && d+dist < result[x_][y_]){
+                    pq.push({d+dist, {x_,y_}});
+                    result[x_][y_]=d+dist;
                 }
             }
-            level++;
         }
-        return -1;
+        if(result[m-1][n-1]==INT_MAX){
+            return -1;
+        }
+        return result[m-1][n-1]+1;
+        
+//         Approach 1 by BFS
+//         int m=grid.size();
+//         int n=grid[0].size();
+        
+//         if(m==0 || n==0 || grid[0][0]!=0){
+//             return -1;
+//         }
+        
+//         auto isSafe=[&](int x, int y){
+//             return x>=0 && x<m && y>=0 && y<n;
+               
+//         };
+//         queue<pair<int, int>> q;
+//         q.push({0,0});
+//         grid[0][0]=1;
+        
+//         int level=0;
+//         while(!q.empty()){
+//             int N=q.size();
+//             while(N--){
+//                 auto curr=q.front();
+//                 q.pop();
+//                 int x=curr.first;
+//                 int y=curr.second;
+                
+//                 if(x==m-1 && y==n-1){
+//                     return level+1;
+//                 }
+//                 for(auto dir: directions){
+//                     int _x = x+dir[0];
+//                     int _y = y+dir[1];
+                    
+//                     if(isSafe(_x,_y) && grid[_x][_y]==0){
+//                         q.push({_x,_y});
+//                         grid[_x][_y]=1;
+//                     }
+//                 }
+//             }
+//             level++;
+//         }
+//         return -1;
     }
 };
