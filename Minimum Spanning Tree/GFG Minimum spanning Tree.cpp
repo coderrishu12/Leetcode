15ecaab5978a4caa940d7348f8dc4382dbe168bd
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-12 12:39:28 +0530

    Create GFG Minimum spanning Tree.cpp

---
+++ Minimum Spanning Tree/GFG Minimum spanning Tree.cpp
@@ -0,0 +1,33 @@
+Time Complexity => O(ElogE)
+
+typedef pair<int, int> P;
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        priority_queue<P, vector<P> , greater<P>> pq;
+        pq.push({0,0});
+        
+        vector<bool> inMST(V, false);
+        int sum=0;
+        while(!pq.empty()){
+            auto p=pq.top();
+            pq.pop();
+            
+            int wt   = p.first;
+            int node = p.second;
+            
+            if(inMST[node]==true){
+                continue;
+            }
+            inMST[node]=true;
+            sum+=wt;
+            for(auto temp: adj[node]){
+                int neighbour    = temp[0];
+                int neighbour_wt = temp[1];
+                if(inMST[neighbour] == false){
+                    pq.push({neighbour_wt , neighbour});
+                }
+            }
+        }
+        return sum;
+       
+    }
