c3a817a531f2b72d84f9df58ce970435f3646b84
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-13 06:15:11 +0530

    Time: 82 ms (66.67%), Space: 45.6 MB (71.98%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<int> parent;
-    vector<int> rank;
     int find(int X){
         if(parent[X]==X){
             return X;
@@ -11,25 +10,13 @@ public:
     
     void Union(int x, int y){
         
-        int parent_x = find(x);
-        int parent_y = find(y);
-        
-        if(parent_x==parent_y){
-            return ;
-        }
-        
-        if(rank[parent_x]<rank[parent_y]){
-            parent[parent_x]=parent_y;
-        }else if(rank[parent_y]<rank[parent_x]){
-            parent[parent_y]=parent_x;
-        }else{
-            parent[parent_y]=parent_x;
-            rank[parent_x]++;
-        }
+        int parent_x=find(x);
+        int parent_y=find(y);
+        if(parent_x!=parent_y) parent[parent_y]=parent_x;
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
         parent.resize(n);
-        rank.resize(n,0);
+
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
