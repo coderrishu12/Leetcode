48bf14ca30851f36069d691dcf17c7618c8e8df5
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-24 06:38:21 +0530

    Time: 10 ms (70.46%), Space: 11.2 MB (63.10%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<int, int> mp;
-        int n = s.length();
-        int len = 0;
-        int maxi = 0;
-        int i=0,j=0;
-        while(j<n){
-            
-            mp[s[j]-'a']++;
-            len++;
-            
-            while(mp[s[j]-'a']>1){
-                mp[s[i]-'a']--;
-                i++;
-                len--;
+//         Approach 2
+        vector<int> hashVec(255,-1);
+        
+        int i=0, j=0;
+        int len=0, maxLen=0;
+        while(j<s.length()){
+            if(hashVec[s[j]]!=-1){
+                if(hashVec[s[j]]>=i){
+                    i = hashVec[s[j]]+1;
+                }
             }
             
-            
+            len=j-i+1;
+            maxLen = max(maxLen, len);
+            hashVec[s[j]] = j;
             j++;
-            maxi = max(maxi,len);
         }
-        return maxi;
+        return maxLen;
+        
+        
+//         Approach 1 
+//         unordered_map<int, int> mp;
+//         int n = s.length();
+//         int len = 0;
+//         int maxi = 0;
+//         int i=0,j=0;
+//         while(j<n){
+            
+//             mp[s[j]-'a']++;
+//             len++;
+            
+//             while(mp[s[j]-'a']>1){
+//                 mp[s[i]-'a']--;
+//                 i++;
+//                 len--;
+//             }
+            
+            
+//             j++;
+//             maxi = max(maxi,len);
+//         }
+//         return maxi;
     }
 };
