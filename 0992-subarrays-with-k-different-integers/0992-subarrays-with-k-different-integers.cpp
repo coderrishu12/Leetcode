db5fb5c19bb3ea475fd6142dfdced7a598b89851
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-28 14:02:17 +0530

    Time: 123 ms (67.31%), Space: 56.2 MB (35.34%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subArray(vector<int>& nums, int k){
+        int i=0, j=0, cnt=0;
+        unordered_map<int, int> mp;
+        while(j<nums.size()){
+            mp[nums[j]]++;
+            
+            while(mp.size()>k){
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0){
+                    mp.erase(nums[i]);
+                }
+                i++;
+            }
+            
+            cnt+=j-i+1;
+            j++;   
+        }
+        return cnt;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return subArray(nums,k) - subArray(nums,k-1);
+    }
+};
