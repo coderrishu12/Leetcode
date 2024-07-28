2a2bb59d86e89aa301ef654ba3b982d3c236a7f2
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-07-28 16:56:43 +0530

    Time: 5 ms (49.11%), Space: 8.2 MB (28.37%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        
+        stack<char> st;
+        for(auto i: s){
+            if(i == '('){
+                if(!st.empty()){
+                    result.push_back(i);
+                }
+                    st.push(i);
+                
+            }else{
+                st.pop();
+                if(!st.empty()){
+                    result.push_back(i);
+                }
+                
+            }
+        }
+        return result;
+        
+    }
+};
