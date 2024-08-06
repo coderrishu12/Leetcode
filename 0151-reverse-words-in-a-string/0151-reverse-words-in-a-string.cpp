fc828b36324e2b3b8a6d33f1cb84ad3c58a30e09
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-08-06 16:12:13 +0530

    Time: 6 ms (46.86%), Space: 8.8 MB (74.98%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        // int n=s.length();
+        // stringstream ss(s);
+        // string token = "";
+        // string result = "";
+        // while(ss>> token){
+        //     result = token + " "+ result;
+        // }
+        // if (!result.empty() && result.back() == ' ') {
+        //     result.pop_back();
+        // }
+        // return result.substr(0,n);
+        
+        reverse(s.begin(), s.end());
+        
+        int l=0, r=0, i=0;
         int n=s.length();
-        stringstream ss(s);
-        string token = "";
-        string result = "";
-        while(ss>> token){
-            result = token + " "+ result;
-        }
-        if (!result.empty() && result.back() == ' ') {
-            result.pop_back();
-        }
-        return result.substr(0,n);
-        
-//         vector<string> vec;
-        
-//         int l=0;
-        
-//         for(int i=0;i<s.length();){
-//             string str = "";
-//             for(int j=i;j<s.length();j++){
-//                 if(s[j]==' '){
-//                     l=j;
-//                     break;
-//                 }
-//                 str+=s[j];
-//             }
-//             vec.push_back(str);
-//             i=l+1;
+        while(i<n){
+            while(i<n && s[i]!=' '){
+                s[r++] = s[i++];
+            }
             
-//         }
-//         reverse(vec.begin(), vec.end());
-//         string str="";
-//         for(int i=0;i<vec.size();i++){
-//             if(i==vec.size()-1){
-//                 str+=vec[i];
-//             }
-//             else{
-//                 str+=(vec[i]+" ");
-//             }
-//         }
-//         return str;
+            if(l<r){
+                reverse(s.begin()+l, s.begin()+r);
+                
+                s[r]=' ';
+                r++;
+                l=r;
+            }
+            i++;
+        }
+        s=s.substr(0,r-1);
+        return s;
     }
 };
