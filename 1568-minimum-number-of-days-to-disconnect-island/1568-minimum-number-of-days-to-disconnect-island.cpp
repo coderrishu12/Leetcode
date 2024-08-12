7005260ce2f5f8eaedaba7da200a7f8ddc7113a7
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-08-12 21:53:49 +0530

    Time: 138 ms (19.50%), Space: 20.2 MB (68.41%) - LeetHub

--- 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
@@ -1,43 +1,58 @@
 class Solution {
-public:
-    int minDays(vector<vector<int>>& grid) {
-        auto count_islands = [&]() -> int {
-            vector<vector<int>> seen(grid.size(), vector<int>(grid[0].size(), 0));
-            int islands = 0;
-            
-            function<void(int, int)> dfs = [&](int r, int c) {
-                seen[r][c] = 1;
-                for (auto [dr, dc] : {pair{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) {
-                    int nr = r + dr, nc = c + dc;
-                    if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && grid[nr][nc] == 1 && !seen[nr][nc]) {
-                        dfs(nr, nc);
-                    }
-                }
-            };
-            
-            for (int i = 0; i < grid.size(); i++) {
-                for (int j = 0; j < grid[0].size(); j++) {
-                    if (grid[i][j] == 1 && !seen[i][j]) {
-                        islands++;
-                        dfs(i, j);
-                    }
-                }
-            }
-            return islands;
-        };
+public: int n, m;
+    
+    void DFS(vector<vector<int>> &grid, int i, int j, vector<vector<bool>>& vis){
+        if(i<0 || i>=m || j<0 || j>=n || vis[i][j] || grid[i][j]==0){
+            return;
+        }
         
-        if (count_islands() != 1) return 0;
+        vis[i][j]=true;
         
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 1) {
-                    grid[i][j] = 0;
-                    if (count_islands() != 1) return 1;
-                    grid[i][j] = 1;
+        DFS(grid , i+1, j, vis);
+        DFS(grid , i-1, j, vis);
+        DFS(grid , i, j+1, vis);
+        DFS(grid , i, j-1, vis);
+    }
+    
+    int numOfIslands(vector<vector<int>> &grid){
+        int islands = 0;
+        
+        vector<vector<bool>> vis(m, vector<bool>(n));
+        
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && !vis[i][j]){
+                    DFS(grid, i, j, vis);
+                    islands++;
                 }
             }
         }
+        return islands;
+    }
+    
+    int minDays(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
         
+        int islands = numOfIslands(grid);
+        if(islands>1 || islands == 0){
+            return 0;
+        }else{
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(grid[i][j]==1){
+                        grid[i][j]=0;
+                        islands = numOfIslands(grid);
+                        
+                        if(islands>1 || islands==0){
+                            return 1;
+                        }
+                        
+                        grid[i][j]=1;
+                    }
+                }
+            }
+        }
         return 2;
     }
 };
