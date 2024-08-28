d4d1bfa5f85400642e515a35db27a569bfa02ff5
coderrishu12 <155959766+coderrishu12@users.noreply.github.com>
2024-08-28 20:27:55 +0530

    Time: 304 ms (31.59%), Space: 222.2 MB (13.29%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,24 +1,58 @@
 class Solution {
 public:
-    bool countSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i , int j){
-        if(i < 0 || i>= grid1.size() || j<0 || j>= grid1[0].size()){
-            return true;
-        }
-        
-        if(grid2[i][j]!=1){
-            return true;
-        }
-        
-        grid2[i][j]=-1;
-        bool result = (grid1[i][j]==1);
-        
-        result = result & countSubIsland(grid1, grid2, i+1, j);
-        result = result & countSubIsland(grid1, grid2, i-1, j);
-        result = result & countSubIsland(grid1, grid2, i, j+1);
-        result = result & countSubIsland(grid1, grid2, i, j-1);
-
-        return result;
+    vector<vector<int>> directions{{1,0}, {-1,0}, {0,1}, {0,-1}};
+    
+     bool countSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i , int j){
+        int m = grid2.size();
+        int n = grid2[0].size();
+         
+         bool result = true;
+         
+         queue<pair<int, int>> q;
+         q.push({i,j});
+         grid2[i][j]=-1;
+         
+         while(!q.empty()){
+             auto [x,y] = q.front();
+             q.pop();
+             
+             if(grid1[x][y]!=1){
+                 result = false;
+             }
+             
+             for(auto &dir: directions){
+                 int newX = x + dir[0];
+                 int newY = y + dir[1];
+                 
+                 if(newX>=0 && newX<m && newY>=0 && newY<n && grid2[newX][newY]==1){
+                        grid2[newX][newY]=-1;
+                        q.push({newX, newY});
+                 }
+             }
+         }
+         return result;
     } 
+    
+    
+//     bool countSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i , int j){
+//         if(i < 0 || i>= grid1.size() || j<0 || j>= grid1[0].size()){
+//             return true;
+//         }
+        
+//         if(grid2[i][j]!=1){
+//             return true;
+//         }
+        
+//         grid2[i][j]=-1;
+//         bool result = (grid1[i][j]==1);
+        
+//         result = result & countSubIsland(grid1, grid2, i+1, j);
+//         result = result & countSubIsland(grid1, grid2, i-1, j);
+//         result = result & countSubIsland(grid1, grid2, i, j+1);
+//         result = result & countSubIsland(grid1, grid2, i, j-1);
+
+//         return result;
+//     } 
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int subIslands = 0;
         int m = grid2.size();
